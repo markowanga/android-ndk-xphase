9f3520e22ffde9a63d39ac92ede69c918a2fd77a
Marcin Wątroba <markowanga@gmail.com>
2021-09-15 07:39:31 +0200

    Update

--- app/src/main/java/pl/theliver/nativeapplicationxphase/MainActivity.kt
+++ app/src/main/java/pl/theliver/nativeapplicationxphase/MainActivity.kt
@@ -1,5 +1,6 @@
 package pl.theliver.nativeapplicationxphase
 
+import android.app.Service
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -11,7 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
